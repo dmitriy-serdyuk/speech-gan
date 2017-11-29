c2559eb1cc6611f040132d81f58404f9a4facc8b
dmitriy-serdyuk <serdyuk.dmitriy@gmail.com>
2017-11-29 17:30:07 -0500

    Add framewise vctk

--- speech_gan/dataset.py
+++ speech_gan/dataset.py
@@ -1,5 +1,6 @@
 from os.path import join, isdir, isfile
 from os import listdir
+from scipy.io.wavfile import read
 
 
 class VCTK(object):
@@ -10,7 +11,7 @@ class VCTK(object):
         wav_path = join(path, 'wav48')
         dirs = [f for f in listdir(txt_path) if isdir(join(txt_path, f))]
         for dir in dirs:
-            self.files.extend((join(txt_path, dir, f), join(wav_path, dir, f))
+            self.files.extend((join(txt_path, dir, f), join(wav_path, dir, f[:-4] + '.wav'))
                               for f in listdir(join(txt_path, dir)) if isfile(join(txt_path, dir, f)))
 
     def __getitem__(self, item):
@@ -18,3 +19,19 @@ class VCTK(object):
 
     def __len__(self):
         return len(self.files)
+
+
+class FramewiseVCTK(object):
+    def __init__(self, dataset, window_size, hop_size):
+        self.total_length = 0
+        for text, wav in dataset:
+            sample_rate, data = read(wav)
+            window_size_frames = sample_rate // 1000 * window_size
+            hop_size_frames = sample_rate // 1000 * hop_size
+            self.total_length += (data.shape[0] - window_size_frames) // hop_size_frames
+
+    def __getitem__(self, item):
+        pass
+
+    def __len__(self):
+        return self.total_length
